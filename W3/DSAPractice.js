70546a279f9108ca398e06c5c382c6a22da91094
MorganGravelat <mgravelat@gmail.com>
2022-05-31 19:46:06 -0400

    Finished with DSA practice

--- W3/DSAPractice.js
+++ W3/DSAPractice.js
@@ -64,3 +64,21 @@ var reverseList = function(head) {
 //         self.nums.append(val)
 //         self.nums.sort()
 //         return self.nums[len(self.nums) - self.k]
+// heapq method
+// there solution to the problem using a tool I did not know existed
+//
+//
+// class KthLargest:
+//     def __init__(self, k: int, nums: List[int]):
+//         self.k = k
+//         self.heap = nums
+//         heapq.heapify(self.heap)
+
+//         while len(self.heap) > k:
+//             heapq.heappop(self.heap)
+
+//     def add(self, val: int) -> int:
+//         heapq.heappush(self.heap, val)
+//         if len(self.heap) > self.k:
+//             heapq.heappop(self.heap)
+//         return self.heap[0] //Much better Memory and Time usage than my version
