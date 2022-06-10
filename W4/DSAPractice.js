c959d07b8954357f0202adc14a9908f856616c42
MorganGravelat <mgravelat@gmail.com>
2022-06-10 18:59:25 -0400

    More DSA practice

--- W4/DSAPractice.js
+++ W4/DSAPractice.js
@@ -16,6 +16,10 @@ function getNthFib(n, memo = {1: 0, 2: 1}) {
         return memo[n];
     }
 }
+
+
+////////////////////////////////////////////////////////
+//INPUT EXAMPLE tournamentWinner([["HTML", "C#"],["C#", "Python"],["Python", "HTML"]], [0, 0, 1])
 //My immediate solution for tournamentWinner
 function tournamentWinner(competitions, results) {
     let winners = {}
@@ -52,4 +56,53 @@ function tournamentWinner(competitions, results) {
     }
 
     return winner;
+  }
+
+  //ADDITIONAL SOLUTION
+  const HOME_TEAM_WON = 1;
+
+function tournamentWinner(competitions, results) {
+  let currentBestTeam = '';
+  const scores = {[currentBestTeam]: 0};
+
+  for (let i = 0; i < competitions.length; i++) {
+    const result = results[i]
+    const [homeTeam, awayTeam] = competitions[i]
+
+    const winningTeam = result === HOME_TEAM_WON ? homeTeam : awayTeam;
+
+    updateScores(winningTeam, 3, scores);
+
+    if (scores[winningTeam] > scores[currentBestTeam]) {
+      currentBestTeam = winningTeam;
+    }
+  }
+  // Write your code here.
+  return currentBestTeam;
+}
+
+function updateScores(team, points,scores) {
+  if (!(team in scores)) scores[team] = 0;
+
+  scores[team] += points;
+}
+
+////////////////////////////////////////////////////////
+//Completed this one in 5 minutes GOOD JOB ME
+
+function isValidSubsequence(array, sequence) {
+    let count = 0;
+
+    for (let i = 0; i < array.length; i++) {
+      let ele = array[i];
+      if (count === sequence.length) break;
+      if (ele === sequence[count]) {
+        count++
+      }
+    }
+    console.log(count);
+    if (count === sequence.length) {
+      return true;
+    }
+    return false;
   }
