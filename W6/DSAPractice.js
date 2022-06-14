101585a2bb5c4de6efb857108ddb6aff32868825
MorganGravelat <mgravelat@gmail.com>
2022-06-14 17:34:27 -0400

    DSA Practice sorted prime numbers with helper isPrime finished.

--- W6/DSAPractice.js
+++ W6/DSAPractice.js
@@ -7,16 +7,25 @@ function sortedSquaredArray(array) {
   }
 //Squaring all numbers in an array and sorting them.
 
-
+//Best Is Prime Number Equation
 function isPrime(num){
     if (num === 2) return true;
-
     if (num % 2 === 0) return false;
-
     if (num < 2) return false;
 
-    for (let i = 3; i <= math.floor(math.sqrt(num)); i+=2) {
+    for (let i = 3; i <= Math.floor(Math.sqrt(num)); i+=2) {
       if (num % i === 0) return false;
     }
     return true;
-}
+  }
+
+  function sortedPrimes(input_array){
+    var result = [];
+    for (let i = 0; i < input_array.length; i++) {
+      let num = input_array[i];
+      if (isPrime(num)) result.push(num);
+    }
+    let typedArray = Float32Array.from(result);
+    let sortedArray = [...typedArray.sort()];
+    return sortedArray;
+  }
