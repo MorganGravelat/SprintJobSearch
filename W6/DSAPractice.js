eb89193d1f9103cf618a6a9d6b3243f66ab703c7
MorganGravelat <mgravelat@gmail.com>
2022-06-14 17:19:27 -0400

    Is Prime Solution

--- W6/DSAPractice.js
+++ W6/DSAPractice.js
@@ -6,3 +6,17 @@ function sortedSquaredArray(array) {
     return array.sort(function(a, b){return a - b});
   }
 //Squaring all numbers in an array and sorting them.
+
+
+function isPrime(num){
+    if (num === 2) return true;
+
+    if (num % 2 === 0) return false;
+
+    if (num < 2) return false;
+
+    for (let i = 3; i <= math.floor(math.sqrt(num)); i+=2) {
+      if (num % i === 0) return false;
+    }
+    return true;
+}
