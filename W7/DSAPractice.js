7e5f69b4b7068fc46b9e0a4cc6a724735c8628d4
MorganGravelat <mgravelat@gmail.com>
2022-06-29 14:24:34 -0400

    Placing algo solutin to the same problem

--- W7/DSAPractice.js
+++ W7/DSAPractice.js
@@ -20,3 +20,34 @@ function smallestDifference(arrayOne, arrayTwo) {
   }
 
   // My solution
+  // Below is his solution
+
+  function smallestDifferenc(arrayOne, arrayTwo) {
+    arrayOne.sort((a,b) => a - b);
+    arrayTwo.sort((a,b) => a - b);
+    let idxOne = 0;
+    let idxTwo = 0;
+    let smallest = Infinity;
+    let current = Infinity;
+    let smallestPair = [];
+
+    while (idxOne < arrayOne.length && idxTwo < arrayTwo.length) { //super smart way to setup a loop for both arrays
+        let firstNum = arrayOne[idxOne];
+        let secondNum = arrayTwo[idxTwo];
+        if (firstNum < secondNum) {
+            current = secondNum - firstNum;
+            idxOne++;
+        } else if (secondNum < firstNum) {
+            current = firstNum - secondNum;
+            idxTwo++;
+        } else {
+            return [firstNum, secondNum];
+        }
+        if (smallest > current) {
+            smallest = current;
+            smallestPair = [firstNum, secondNum];
+        }
+    }
+    return smallestPair;
+
+}
