49c7b010cab68835e9c494be17e5f3c2cab46ca0
MorganGravelat <mgravelat@gmail.com>
2022-07-07 15:21:25 -0400

    Learning about Python Data Types

--- UdemyPythonNotes/Udemy.py
+++ UdemyPythonNotes/Udemy.py
@@ -6,3 +6,20 @@ def name_length():
     print(len(name))
     return len(name)
 #Above is the worse version I made
+
+# Primitive Data Types
+
+#Print a string and index that string
+print ("Hello"[0])
+print("123" + "345")
+
+#Type of data in python # this means it prints out to be this
+num_char = 1
+print(type(num_char)) # <class 'int'>
+
+
+#Convert num_char to string
+new_num_char = str(num_char)
+
+#Can be used now in a concat print statement
+print('HEY FOOL' + new_num_char) #type(new_num_char) <class 'str'>
