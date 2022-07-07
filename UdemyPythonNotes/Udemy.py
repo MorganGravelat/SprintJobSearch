74590e175d45ed2238a89e37e6d043897b58bc19
MorganGravelat <mgravelat@gmail.com>
2022-07-07 13:44:01 -0400

    Added my function for the python input practice

--- UdemyPythonNotes/Udemy.py
+++ UdemyPythonNotes/Udemy.py
@@ -1,1 +1,8 @@
 print( len( input("What is your name? ") ) ) #This is a way you can prompt What is your name? Morgan and when I answer my name it spits out 6
+
+def name_length():
+    print('What is your name?')
+    name = input()
+    print(len(name))
+    return len(name)
+#Above is the worse version I made
