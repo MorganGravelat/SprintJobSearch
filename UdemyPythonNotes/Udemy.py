1bed657bfea3a5e766f524ccb49b55c8412fd44b
MorganGravelat <mgravelat@gmail.com>
2022-07-11 17:09:46 -0400

    Finishing up day 3 udemy python course

--- UdemyPythonNotes/Udemy.py
+++ UdemyPythonNotes/Udemy.py
@@ -1,3 +1,6 @@
+import this
+
+
 print( len( input("What is your name? ") ) ) #This is a way you can prompt What is your name? Morgan and when I answer my name it spits out 6
 
 def name_length():
@@ -168,3 +171,158 @@ Number	Format	Output	Description
 13	{:<10d}	13	Left aligned (width 10)
 13	{:^10d}	    13	Center aligned (width 10)
 '''
+
+
+### Control Flow and Logical Operators
+
+#if else python code
+'''
+if condition:
+    do this
+else:
+    do this
+'''
+
+#Example if statement
+print ("Welcome to the rollercoaster")
+height = int(input("What is your height in cm? "))
+if height > 120: # You can also use >= / <= / < / == / != # All simple comparison operators
+    print("You can ride the rollercoaster")
+else:
+    print("sorry go away")
+
+
+### Mathematical Operators
+## Modulo
+# Modulo operator can be used to detect odds and evens
+number = int(input("Which number do you want to check? "))
+
+def OddorEven(num):
+      if num % 2 != 0:
+    print("This is an odd number")
+    return "This is an odd number"
+  else:
+    print("This is an even number")
+
+OddorEven(number)
+
+##Updated rollercoaster height checker
+print ("Welcome to the rollercoaster")
+height = int(input("What is your height in cm? "))
+if height > 120: # You can also use >= / <= / < / == / != # All simple comparison operators Conditional Operators Conditions
+    print("You can ride the rollercoaster")
+    age = int(input("What is your age? "))
+    if age < 12:
+        print("Please pay $5.")
+    elif age <= 18:
+        print("Please pay $7.")
+    else:
+        print("Please pay $12.")
+else:
+    print("sorry go away")
+######## BREAK
+
+
+### Upgraded BMI Calculator
+height = float(input("enter your height in m: "))
+weight = float(input("enter your weight in kg: "))
+
+def bmiCalculator(height, weight):
+  bmi = round(weight / (height ** 2))
+
+  if bmi < 18.5:
+    print(f"Your BMI is {bmi}, you are underweight.")
+  elif bmi <= 25:
+    print(f"Your BMI is {bmi}, you have a normal weight.")
+  elif bmi <= 30:
+    print(f"Your BMI is {bmi}, you are slightly overweight.")
+  elif bmi <= 35:
+    print(f"Your BMI is {bmi}, you are obese.")
+  else:
+    print(f"Your BMI is {bmi}, you are clinically obese.")
+  return
+
+
+bmiCalculator(height, weight)
+
+### End of BMI Calculator
+
+##Found out if something is a leap year modulo if else statements
+year = int(input("Which year do you want to check? "))
+
+if year % 100 == 0:
+  if year % 400 == 0:
+    print("Leap Year")
+  else:
+    print("Not Leap Year")
+elif year % 4 == 0:
+  print("Leap Year")
+else:
+  print("Not Leap Year")
+
+##Their version below
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("Leap Year")
+        else:
+            print("Not leap year.")
+    else:
+        print("Leap Year.")
+else:
+    print("Not Leap Year.")
+
+### END OF Leap Year Finder
+##If elif else practice
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height >= 120:
+  print("You can ride the rollercoaster!")
+  age = int(input("What is your age? "))
+  if age < 12:
+    bill = 5
+    print("Child tickets are $5.")
+  elif age <= 18:
+    bill = 7
+    print("Youth tickets are $7.")
+  else:
+    bill = 12
+    print("Adult tickets are $12.")
+
+  wants_photo = input("Do you want a photo taken? Y or N. ")
+  if wants_photo == "Y":
+    bill += 3
+
+  print(f"Your final bill is ${bill}")
+
+else:
+  print("Sorry, you have to grow taller before you can ride.")
+
+
+##Else if practice // PIZZA ORDER Input if else elif mathematical operator
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
+bill = 0
+
+if size == 'L':
+  bill += 25
+elif size == 'M':
+  bill += 20
+elif size == 'S':
+  bill += 15
+
+if add_pepperoni == "Y":
+  if size == 'S':
+    bill += 2
+  else:
+    bill += 3
+
+if extra_cheese == "Y":
+  bill += 1
+
+print(f"Your final bill is: {bill}")
