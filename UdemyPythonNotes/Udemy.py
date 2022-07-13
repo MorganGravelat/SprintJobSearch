98a9714c217c8d7a488ae4c049d8da6a44fa3bb6
MorganGravelat <mgravelat@gmail.com>
2022-07-13 15:35:24 -0400

    Learning about random and lists

--- UdemyPythonNotes/Udemy.py
+++ UdemyPythonNotes/Udemy.py
@@ -1,3 +1,4 @@
+from secrets import randbelow
 import this
 
 
@@ -390,3 +391,129 @@ elif score > 40 and score < 50:
   print(f"Your score is {score}, you are alright together.")
 else:
   print(f"Your score is {score}")
+
+### Choose  your own adventure
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#Write your code below this line 👇
+
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if choice1 == "left":
+  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+  if choice2 == "wait":
+    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+    if choice3 == "red":
+      print("It's a room full of fire. Game Over.")
+    elif choice3 == "yellow":
+      print("You found the treasure! You Win!")
+    elif choice3 == "blue":
+      print("You enter a room of beasts. Game Over.")
+    else:
+      print("You chose a door that doesn't exist. Game Over.")
+  else:
+    print("You get attacked by an angry trout. Game Over.")
+else:
+  print("You fell into a hole. Game Over.")
+
+
+### Random
+import random #You imust import random
+
+random_integer = random.randint(1, 10) #Random int betwee nthese numbers
+random_float = random.random()*5 # random.random gives float between 0 and 1 * 5 is now 0 and 5
+print(random_integer)
+
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}")
+
+random_side = random.randint(0, 1) ##Coin flip random integer
+if random_side == 1:
+    print("Heads")
+else:
+    print("Tails")
+####################################################################
+
+# Data types
+## lists
+fruits = ["apple", "orange","pear"] #List data structure
+
+## List of states
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey",
+                     "Georgia", "Connecticut", "Massachusetts",
+                     "Maryland", "South Carolina", "New Hampshire",
+                     "Virginia", "New York", "North Carolina",
+                     "Rhode Island", "Vermont", "Kentucky", "Tennessee",
+                     "Ohio", "Louisiana", "Indiana", "Mississippi",
+                     "Illinois", "Alabama", "Maine", "Missouri", "Arkansas",
+                     "Michigan", "Florida", "Texas", "Iowa", "Wisconsin",
+                     "California", "Minnesota", "Oregon", "Kansas",
+                     "West Virginia", "Nevada", "Nebraska", "Colorado",
+                     "North Dakota", "South Dakota", "Montana", "Washington",
+                     "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico",
+                     "Arizona", "Alaska", "Hawaii"]
+
+#Change a variable in the list
+states_of_america[1] = "Pencilvania"
+#You can append items to the list in python
+states_of_america.append("Morganland") # Append Method add to a list
+states_of_america.extend(["Tonyland", "Troyland"]) #Add a list to a list Extend method
+print(states_of_america) #You will notice the list as changed
+## List of fruits
+dirty_dozen = ["Strawberries", "Spinach", "Kale",
+               "Nectarines", "Apples", "Grapes",
+               "Peaches", "Cherries", "Pears",
+               "Tomatoes", "Celery", "Potatoes"]
+## INDEX INFORMATION
+# the 0 is the count of the offset of the item, the Strawberries item is offset by 0 because it begins the list.add()
+
+#Application of Random method , len() function, and string formatting
+##Decide whos going to pay the meal randomly
+
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+
+import random
+
+print(f"{names[random.randint(0, (len(names)-1))]} is going to buy the meal today!")
+
+
+#Below is their version of above
+import random
+
+# Split string method
+names_string = input("Give me everybody's names, seperated by a comma. ")
+names = names_string.split(", ")
+
+#Write your code below this line 👇
+
+#Get the total number of items in list.
+num_items = len(names)
+#Generate random numbers between 0 and the last index.
+random_choice = random.randint(0, num_items - 1)
+#Pick out random person from list of names using the random number.
+person_who_will_pay = names[random_choice]
+
+print(person_who_will_pay + " is going to buy the meal today!")
