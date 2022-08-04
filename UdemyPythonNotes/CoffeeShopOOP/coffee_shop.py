a81477918bb6e876ef22ecc20a7e2450111d3603
MorganGravelat <mgravelat@gmail.com>
2022-08-04 05:06:25 -0400

    Coffee Shop OOP practice

---
+++ UdemyPythonNotes/CoffeeShopOOP/coffee_shop.py
@@ -0,0 +1,37 @@
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
+from menu import Menu, MenuItem
+
+class CoffeeShop:
+    """Models each Part of the Shop"""
+    def __init__(self):
+        self.menu = Menu()
+        self.coffee_maker = CoffeeMaker()
+        self.money_machine = MoneyMachine()
+
+    def add_to_menu(self, name, cost, water, milk, coffee):
+        item = MenuItem(name, cost, water, milk, coffee)
+        self.menu.menu.append(item)
+    def make_order(self):
+        is_on = True
+        while is_on:
+            print(self.menu.get_items())
+            order = input("Please pick a beverage from these options! ").lower()
+            if order == "report":
+                self.coffee_maker.report()
+                self.money_machine.report()
+                continue
+            elif order == "off":
+                is_on = False
+                print('\n🤖 Powering Down 🤖')
+                continue
+            order = self.menu.find_drink(order)
+            if not order:
+                continue
+            available = self.coffee_maker.is_resource_sufficient(order)
+            if not available:
+                continue
+            payment = self.money_machine.make_payment(order.cost)
+            if not payment:
+                continue
+            self.coffee_maker.make_coffee(order)
---
+++ UdemyPythonNotes/CoffeeShopOOP/main.py
@@ -0,0 +1,7 @@
+from coffee_shop import CoffeeShop
+
+
+my_shop = CoffeeShop()
+my_shop.add_to_menu("trash", 100, 100, 25, 0.25)
+
+my_shop.make_order()
