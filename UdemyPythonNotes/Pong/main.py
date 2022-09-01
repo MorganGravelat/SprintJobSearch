846974c4086972270ff3e6d53110fd9fda287e38
MorganGravelat <mgravelat@gmail.com>
2022-08-31 23:57:59 -0400

    Pong Python Udemy

---
+++ UdemyPythonNotes/Pong/main.py
@@ -0,0 +1,21 @@
+# paddle
+# ball
+#enempy paddle which inherits from paddle
+#Create the screen
+#Create and move a paddle
+#Create another paddle
+#create the ball and make it move
+#detect collision with wall and bounce
+# Detect collision with paddle
+#Detect when paddle misses
+#Keep score
+from turtle import Screen
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong")
+#screen.tracer(0)
+
+
+screen.exitonclick()
