e9249a032ca1cdd71d56275c8b455e349b34af07
MorganGravelat <mgravelat@gmail.com>
2022-09-28 22:10:56 -0400

    Submitting some work for green

--- UdemyPythonNotes/UdemyF20.py
+++ UdemyPythonNotes/UdemyF20.py
@@ -2678,3 +2678,48 @@ while game_on:
     screen.update()
 
 screen.exitonclick()
+
+#Inheriting a class like the paddle
+#Paddle.py
+from turtle import Turtle
+
+
+class Paddle:
+    def __init__(self,posX, posY):
+        self.paddle = Turtle()
+        self.paddle.up()
+        self.paddle.goto(posX, posY)
+        self.paddle.shape("square")
+        self.paddle.color("white")
+        self.paddle.shapesize(stretch_wid=5, stretch_len=1)
+
+    def up(self):
+        (posX, posY) = self.paddle.pos()
+        if (posY >= 220):
+            return
+        else:
+            print(posX, posY)
+            self.paddle.setpos(posX, (posY+20))
+
+    def down(self):
+        (posX, posY) = self.paddle.pos()
+        if (posY <= -220):
+            return
+        else:
+            print(posX, posY)
+            self.paddle.setpos(posX, (posY - 20))
+##Ball.py
+from turtle import Turtle
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("white")
+        self.shape("circle")
+        self.penup()
+
+    def move(self):
+        new_x = self.xcor() + 8
+        new_y = self.ycor() + 8
+        self.goto(new_x, new_y)
