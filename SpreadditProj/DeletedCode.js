fa76bae3d75a38046baf1561426176caa4f64373
MorganGravelat <mgravelat@gmail.com>
2022-10-08 03:42:57 -0400

    Added a couple deleted code snipppepets

---
+++ SpreadditProj/DeletedCode.js
@@ -0,0 +1,6 @@
+//<LikeButton post={post} likesCount={likesCount} likeInfo={likeInfo}/>
+                {/* <div className='space-between' /> */}
+                  {/* <NavLink className="post-image-container" exact to={`/posts/${post.post_id ? post?.post_id : post?.id}`}>
+                    <img className="post-image" src={post.image_url ? post?.image_url : post?.post_image_url} alt="post" />
+                  </NavLink> */}
+//post_id = post.post_id ? post.post_id : post.id;
