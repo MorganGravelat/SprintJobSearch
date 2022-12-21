9d6814cfc24b1a306aa53ca0c6ac5caa3bddbe44
MorganGravelat <mgravelat@gmail.com>
2022-12-20 23:39:02 -0500

    DSA Problem

--- AlgoExpert/DSAProblems.js
+++ AlgoExpert/DSAProblems.js
@@ -379,3 +379,23 @@ def convert_time(time):
         return f"{hrs:02d}:{mins:02d}"
 
 //// Calender Matching END
+//// Selection Sort BEGIN
+function selectionSort(array) {
+    let startIdx = 0;
+    while (startIdx < array.length - 1) {
+        let smallestIdx = startIdx;
+        for (let i = startIdx + 1; i < array.length; i++) {
+            if (array[smallestIdx] > array[i]) smallestIdx = i;
+        }
+        swap(startIdx, smallestIdx, array);
+        startIdx++;
+    }
+    return array;
+}
+
+function swap(i, j, array) {
+    const temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
+}
+//// Selection Sort END
