dad4816ce76cc71113acfb980065b5aa225e3bc2
MorganGravelat <mgravelat@gmail.com>
2022-12-23 21:26:20 -0500

    Insertion Sort

--- AlgoExpert/DSAProblems.js
+++ AlgoExpert/DSAProblems.js
@@ -417,3 +417,15 @@ function swap(i, j, array) {
     array[i] = temp;
 }
 /////////////MOVE ELEMENT TO END END
+/////INSERTTION SORT BEGIN
+function insertionSort(array) {
+    for(let i = 1; i < array.length; i++) {
+      let j = i;
+      while(j > 0 && array[j] < array[j-1]) {
+        [array[j],array[j-1]] = [array[j-1], array[j]]
+        j -= 1;
+      }
+    }
+    return array;
+  }
+/////INSERTTION SORT END
