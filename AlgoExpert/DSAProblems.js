83f784dcfd056618c208d02580b1355b301d48b4
MorganGravelat <mgravelat@gmail.com>
2022-12-23 21:38:17 -0500

    Semordnilap Pairs DSA

--- AlgoExpert/DSAProblems.js
+++ AlgoExpert/DSAProblems.js
@@ -429,3 +429,21 @@ function insertionSort(array) {
     return array;
   }
 /////INSERTTION SORT END
+//////Semordnilap BEGIN
+
+function semordnilap(words) {
+    const wordsSet = new Set(words);
+    const semordnilapPairs = [];
+
+    for (const word of words) {
+      const reverse = word.split('').reverse().join('');
+      if (wordsSet.has(reverse) && reverse !== word) {
+        semordnilapPairs.push([word, reverse]);
+        wordsSet.delete(word);
+        wordsSet.delete(reverse);
+      }
+    }
+
+    return semordnilapPairs;
+  }
+///////Semordnilap END
