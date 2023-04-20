3102631b45859040d96ad1ece0cfb01b1ae91408
MorganGravelat <mgravelat@gmail.com>
2023-04-20 17:16:24 -0400

    Leet code and Algo Expert

--- AlgoExpert/DSAProblems.js
+++ AlgoExpert/DSAProblems.js
@@ -873,3 +873,39 @@ function longestCommonSubsequence(str1, str2) {
 //                 return ListNode(digit, self.helper(l1, l2.next, False))
 
 //         return None
+
+
+/*
+function cycleInGraph (edges) {
+const numberOfNodes =
+edges.length;
+}
+const visited = new Array(numberOfNodes).fill(false);
+const currentlyInStack = new Array(numberOfNodes).fill(false);
+for (let node = 0; node < numberOfNodes; node++) {
+}
+if (visited [node]) continue;
+const containsCycle = isNodeInCycle(node, edges, visited, currentlyInStack); if (containsCycle) return true;
+return false;
+function isNodeInCycle (node, edges, visited, currentlyInStack) {
+visited[node] = true;
+currentlyInStack [node ] = true;
+const neighbors = edges [node]; for (const neighbor of neighbors) { if (!visited [neighbor]) {
+const containsCycle
+isNodeInCycle(neighbor, edges, visited, currentlyInStack);
+if (containsCycle) return true;
+} else if (currentlyInStack [neighbor]) { return true;
+}
+}
+}
+currentlyInStack[node] = false; return false;
+
+Find the score of all prefixes of an Array
+def findPrefixScore(self, nums: List[int]) -> List[int]:
+        m=nums[0]
+        nums[0]*=2
+        for i in range(1,len(nums)):
+            m = max(m, nums[i])
+            nums[i] += nums[i-1] + m
+        return nums
+*/
