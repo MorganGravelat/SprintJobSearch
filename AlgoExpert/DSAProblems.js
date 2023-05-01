11c900b18a17011e7ae6509386b6ce16d6cf1dcf
MorganGravelat <mgravelat@gmail.com>
2023-04-30 23:04:07 -0400

    Minimum Path Sum JS solution

--- AlgoExpert/DSAProblems.js
+++ AlgoExpert/DSAProblems.js
@@ -1047,3 +1047,58 @@ select sale_date, sum(case when fruit='apples' then sold_num else -sold_num end)
 from sales
 group by sale_date
 */
+
+Minimum Path Sum
+Medium
+10.7K
+138
+company
+Google
+company
+Amazon
+company
+Bloomberg
+Given a m x n grid filled with non-negative numbers, find a path from top left to bottom right, which minimizes the sum of all numbers along its path.
+
+Note: You can only move either down or right at any point in time.
+
+
+
+Example 1:
+
+
+Input: grid = [[1,3,1],[1,5,1],[4,2,1]]
+Output: 7
+Explanation: Because the path 1 → 3 → 1 → 1 → 1 minimizes the sum.
+Example 2:
+
+Input: grid = [[1,2,3],[4,5,6]]
+Output: 12
+
+
+Constraints:
+
+m == grid.length
+n == grid[i].length
+1 <= m, n <= 200
+0 <= grid[i][j] <= 100
+
+//Miniumum Path Sum in a grid ANSWER
+var minPathSum = function(grid) {
+    let row = grid.length;
+    let col = grid[0].length;
+    for(let i=0;i<row;i++) {
+        for(let j=0;j<col;j++) {
+            if(i == 0 && j== 0) {
+                continue
+            } else if( i== 0) {
+                grid[i][j] += grid[i][j-1]
+            } else if ( j== 0) {
+                grid[i][j] += grid[i-1][j]
+            } else {
+                grid[i][j] += Math.min(grid[i-1][j],grid[i][j-1])
+            }
+        }
+    }
+    return grid[row-1][col-1]
+};
