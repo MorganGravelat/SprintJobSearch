49abb5e830a46e168e62be7b0fda90f44a826203
MorganGravelat <mgravelat@gmail.com>
2023-05-04 23:49:02 -0400

    Pushing dsa problem

--- AlgoExpert/DSAProblems.js
+++ AlgoExpert/DSAProblems.js
@@ -1102,3 +1102,38 @@ var minPathSum = function(grid) {
     }
     return grid[row-1][col-1]
 };
+/*
+There are n gas stations along a circular route, where the amount of gas at the ith station is gas[i].
+
+You have a car with an unlimited gas tank and it costs cost[i] of gas to travel from the ith station to its next (i + 1)th station. You begin the journey with an empty tank at one of the gas stations.
+
+Given two integer arrays gas and cost, return the starting gas station's index if you can travel around the circuit once in the clockwise direction,
+otherwise return -1. If there exists a solution, it is guaranteed to be unique
+*/
+var canCompleteCircuit = function(gas, cost) {
+    let step = 0;
+    let currentPos = 0;
+    let currentGas = gas[currentPos];
+    let numberOfTimeTried = 1;
+    let output = 0;
+
+    const getNextPos = (curr) => curr === gas.length - 1 ? 0 : ++curr;
+
+    while(step <= gas.length) {
+        if (numberOfTimeTried > gas.length) return -1;
+        if (currentGas < cost[currentPos]) {
+            currentPos = getNextPos(currentPos);
+            numberOfTimeTried += step + 1;
+            step = 0;
+            currentGas = gas[currentPos];
+            output = currentPos;
+            continue;
+        }
+        currentGas -= cost[currentPos];
+        currentPos = getNextPos(currentPos);
+        currentGas += gas[currentPos];
+        step++;
+    }
+
+    return output;
+};
