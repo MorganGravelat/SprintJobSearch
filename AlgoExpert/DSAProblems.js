a58045b8a73e737eff83c5435634aa591764fb9a
Morgan Gravelat <mgravelat@gmail.com>
2023-05-21 23:33:06 -0400

    Update DSAProblems.js
    Did Find Celebrity Leetcode problem.

--- AlgoExpert/DSAProblems.js
+++ AlgoExpert/DSAProblems.js
@@ -1301,3 +1301,53 @@ class Solution:
         while fast.next: fast, slow = fast.next, slow.next
         slow.next = slow.next.next
         return head// python solution
+
+/*
+Suppose you are at a party with n people labeled from 0 to n - 1 and among them, there may exist one celebrity. The definition of a celebrity is that all the other n - 1 people know the celebrity, but the celebrity does not know any of them.
+
+Now you want to find out who the celebrity is or verify that there is not one. You are only allowed to ask questions like: "Hi, A. Do you know B?" to get information about whether A knows B. You need to find out the celebrity (or verify there is not one) by asking as few questions as possible (in the asymptotic sense).
+
+You are given a helper function bool knows(a, b) that tells you whether a knows b. Implement a function int findCelebrity(n). There will be exactly one celebrity if they are at the party.
+
+Return the celebrity's label if there is a celebrity at the party. If there is no celebrity, return -1.
+        */
+       //Find the Celebrity
+       /*
+def findCelebrity(self, n): //Python Version
+    x = 0
+    for i in xrange(n):
+        if knows(x, i):
+            x = i
+    if any(knows(x, i) for i in xrange(x)):
+        return -1
+    if any(not knows(i, x) for i in xrange(n)):
+        return -1
+    return x
+       */
+
+    // Javascript SOlution
+    var solution = function(knows) {
+        /**
+         * @param {integer} n Total people
+         * @return {integer} The celebrity
+         */
+        return function(n) {
+            let c = 0;
+            for (let i=1;i<n;i++) {
+                if (knows(c,i)) {
+                    c = i;
+                }
+            }
+            
+            for (let i=0;i<n;i++) {
+                if (i!=c) {
+                    if (!knows(i,c) || knows(c,i)) {
+                        return -1;
+                    }
+                }
+            }
+            
+            return c;
+            
+        };
+    };
