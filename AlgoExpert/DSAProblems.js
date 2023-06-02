ffd5665372f1ef612c779cf6e9c7578a8b653633
MorganGravelat <mgravelat@gmail.com>
2023-06-02 17:01:22 -0400

    DSA Min Stack

--- AlgoExpert/DSAProblems.js
+++ AlgoExpert/DSAProblems.js
@@ -1409,7 +1409,7 @@ class Solution:
         return res
 
 /*
-105. Construct Binary Tree from Preorder and Inorder Traversal
+Construct Binary Tree from Preorder and Inorder Traversal
 Given two integer arrays preorder and inorder where preorder is the preorder traversal
 of a binary tree and inorder is the inorder traversal of the same tree,
 construct and return the binary tree.
@@ -1426,5 +1426,75 @@ const buildTree = (preorder, inorder) => {
   root.left = buildTree(preorder.slice(1, mid + 1), inorder.slice(0, mid));
   root.right = buildTree(preorder.slice(mid + 1), inorder.slice(mid + 1));
   return root;
-};
+
+
+  Min Stack
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+Implement the MinStack class:
+
+MinStack() initializes the stack object.
+void push(int val) pushes the element val onto the stack.
+void pop() removes the element on the top of the stack.
+int top() gets the top element of the stack.
+int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+/**
+// var MinStack = function() {
+//   this.elements = [];
+// };
+
+// /**
+
+//  @param {number} x
+//  @return {void}
+//  */
+//  MinStack.prototype.push = function(x) {
+//     this.elements.push({
+//       value: x,
+//       min: this.elements.length === 0 ? x : Math.min(x, this.getMin()),
+//     });
+//   };
+//   /**
+
+//    @return {void}
+//    */
+//   MinStack.prototype.pop = function() {
+//     this.elements.pop();
+//   };
+//   /**
+
+//    @return {number}
+//    */
+//   MinStack.prototype.top = function() {
+//     return this.elements[this.elements.length - 1].value;
+//   };
+//   /**
+
+//    @return {number}
+//    */
+//   MinStack.prototype.getMin = function() {
+//     return this.elements[this.elements.length - 1].min;
+//   };
+
+// };
+
 */
