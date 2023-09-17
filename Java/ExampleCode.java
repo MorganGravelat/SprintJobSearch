c75cc396cdafb0d97d15fea54ffb8d5eab0e5144
MorganGravelat <mgravelat@gmail.com>
2023-09-16 20:26:59 -0400

    Finsihing up personal small project 1 with working code printout

--- Java/ExampleCode.java
+++ Java/ExampleCode.java
@@ -59,6 +59,41 @@ public class main {
 		double cost1, cost2;
 		Scanner stringScanner = new Scanner(System.in);
 
+        System.out.print("Enter the Student's Id: ");
+		student_id = stringScanner.nextLine();
+		System.out.print("Enter the Student's full name: ");
+		full_name = stringScanner.nextLine();
+
+		System.out.print("\nEnter crn/credit hours for the first class(like 5665/3): ");
+		first_class = stringScanner.nextLine();
+		class_one_arr = first_class.split("/");
+		crn1 = class_one_arr[0];
+		credits1 = Integer.parseInt(class_one_arr[1]);
+
+		System.out.print("Enter crn/credit hours for the second class(like 5665/3): ");
+		second_class = stringScanner.nextLine();
+		class_two_arr = second_class.split("/");
+		crn2 = class_two_arr[0];
+		credits2 = Integer.parseInt(class_two_arr[1]);
+		//System.out.println(student_id + " " + full_name + " FirstClass" + first_class + " SecondClass" + second_class);
+
+		cost1 = credits1 * 120.25;
+		cost2 = credits2 * 120.25;
+
+		System.out.print("\nThank you!\nFee invoice prepared for: " + full_name);
+		//System.out.print(full_name);
+
+		System.out.print("\n\n		SIMPLE COLLEGE\n		ORLANDO FL 10101\n		"
+				+ "*************************		\n\n		Fee Invoice Prepared for:\n"
+				+ "		[" + full_name + "]" + " [" + student_id + "]\n\n"
+				+ "		1 Credit Hour = $120.25");
+
+		System.out.printf("\n\n		%-8s %-6s", "CRN", "CREDIT HOURS");
+		System.out.printf("\n		%-8s %-6d 		$%-10.2f", crn1, credits1, cost1);
+		System.out.printf("\n		%-8s %-6d 		$%-10.2f", crn2, credits2, cost2);
+		System.out.printf("\n\n			 %10s", "Health & id fees	$35.00");
+		System.out.printf("\n\n		----------------------------------------");
+		System.out.printf("\n			Total Payments" + "		$%.2f", (cost1 + cost2 + 35));
 
 	}
 
