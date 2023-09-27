07e6663fdf433fecf73009cc9c1466f32f1f0571
MorganGravelat <mgravelat@gmail.com>
2023-09-27 17:16:14 -0400

    Three DSA problems on algo expert

--- AlgoExpert/NewDSAPRACTICE.js
+++ AlgoExpert/NewDSAPRACTICE.js
@@ -44,3 +44,85 @@ function isValidSubsequence(array, sequence) { //
    }
   return false;
 }
+function sortedSquaredArray(array) {
+    if (array.length < 1) return [];
+    for (let i = 0; i < array.length; i++) {
+      array[i] = array[i]*array[i];
+    }
+    return array.sort((a,b) => a-b);
+  }
+
+//Tournament winner
+function tournamentWinner(competitions, results) {
+    // Make an object for each winner going through in a loop and then just loop the object for the highest
+    let winnerObj = {};
+    let winner = "";
+    let hiScore = 0;
+    for (let i = 0; i < competitions.length; i++) {
+      if (results[i] === 0) {
+        let win = competitions[i][1]
+        if (win in winnerObj) {
+          winnerObj[win]++;
+          if (winnerObj[win] > hiScore) {
+            hiScore = winnerObj[win];
+            winner = win;
+          }
+        }
+        else {
+          winnerObj[win] = 1;
+          if (winnerObj[win] > hiScore) {
+            hiScore = winnerObj[win];
+            winner = win;
+          }
+
+        }
+      }
+      else {
+        let win = competitions[i][0]
+        if (win in winnerObj) {
+          winnerObj[win]++;
+          if (winnerObj[win] > hiScore) {
+            hiScore = winnerObj[win];
+            winner = win;
+          }
+        }
+        else {
+          winnerObj[win] = 1;
+          if (winnerObj[win] > hiScore) {
+            hiScore = winnerObj[win];
+            winner = win;
+          }
+        }
+      }
+    }
+    // for (ele in winnerObj) {
+    //   let score =
+    //   if (winnerObj[ele] > hiScore) hiSco
+    // }
+    return winner;
+  }
+//// Non-Constructible Change
+function nonConstructibleChange(coins) { //This is O(nlogn) time and O(1) space
+    coins.sort((a,b) => a-b);
+    //console.log(coins);
+    let currentChangeCreated = 0;
+    for (const coin of coins) { // 1,1,2,3,5,7,22
+      if (coin > currentChangeCreated + 1) return currentChangeCreated + 1;
+
+      currentChangeCreated += coin;
+    }
+
+    return currentChangeCreated + 1;
+  }
+///Transpose Matrix
+function transposeMatrix(matrix) {
+    const transposedMatrix  = [];
+    for (let col = 0; col < matrix[0].length; col++) {
+      const newRow=[];
+      for (let row = 0; row < matrix.length; row++) {
+        newRow.push(matrix[row][col]);
+      }
+      transposedMatrix.push(newRow);
+    }
+    return transposedMatrix;
+  }
