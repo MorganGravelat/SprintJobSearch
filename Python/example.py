506db93c061ef75b5cb26f2bde2eb41929cb8e88
MorganGravelat <mgravelat@gmail.com>
2023-10-08 07:47:42 -0400

    Finished the hill cipher python project

--- Python/example.py
+++ Python/example.py
@@ -109,3 +109,115 @@ def main():
     print(encryptedMessage)
     print('Decrypted message:')
     print(decryptedMessage)
+###################################
+'''============================================================================
+| Assignment: pa01 - Encrypting a plaintext file using the Hill cipher
+|
+| Author: Morgan Gravelat
+| Language: python
+|
+| To Execute: python3 pa01.py kX.txt pX.txt
+| where kX.txt is the keytext file
+| and pX.txt is plaintext file
+| Note:
+| All input files are simple 8 bit ASCII input
+| All execute commands above have been tested on Eustis
+|
+| Class: CIS3360 - Security in Computing - Fall 2023
+| Instructor: McAlpin
+| Due Date: October 8th, 2023
++==========================================================================='''
+
+import sys
+import os
+
+# This function reads through the matrix filename called file_name and returns an array of arrays of each row of the matrix read from the file
+def key_matrix_array(file_name):
+    file = open(file_name, 'r') #open opens a file and returns it as a file object 'r' stands for the mode read
+    n = int(file.readline()) #reads the first line of the key matrix file and converts it to an integer
+    key_matrix = []
+    for _ in range(n): #The key matrix has n rows specified at the top and n is iterating through the number of rows
+        row = list(map(int, file.readline().split())) #Each time you call readline() it reads the next line of the file so all you need is n  to iterate the number of times the matrix specifies
+        # the map function runs the same function on each piece of data created by split and turns them into ints shoving them into a row array
+        key_matrix.append(row) #appends the row to the key matrix array
+    file.close() #this method closes the file
+    return key_matrix #returns the key matrix array
+
+# this function goes throug the provided plaintext file and turns it into a string of all lowercase letters
+def read_plaintext(filename):
+    with open(filename, 'r') as file:
+        plaintext = file.read()
+    # Remove characters that are not alphabetic and not part of the English alphabet
+    cleaned_text = ''.join(char.lower() for char in plaintext if char.isalpha() and ((ord('a') <= ord(char) <= ord('z')) or (ord('A') <= ord(char) <= ord('Z'))))
+    return cleaned_text
+
+# This function complies with the instructions and pads the plaintext with x's to make it comply with key size
+def pad_plaintext(plaintext, key_size):
+    # takes the key size which is the number of rows in the key matrix and subtracts the length of the plaintext
+    padding_length = key_size - (len(plaintext) % key_size)
+    if padding_length > 0:
+        plaintext += 'x' * padding_length
+    return plaintext
+
+def encrypt(key_matrix, plaintext):
+    key_size = len(key_matrix)
+    ciphertext = ""
+    for i in range(0, len(plaintext), key_size):
+        block = plaintext[i:i+key_size]
+        encrypted_block = []
+        for row in key_matrix:
+            result = 0
+            for k, char in enumerate(block):
+                result += row[k] * (ord(char) - ord('a'))
+            encrypted_block.append(result % 26 + ord('a'))
+        ciphertext += ''.join([chr(char) for char in encrypted_block])
+    return ciphertext
+
+# Function that finds file by name in the currrent folder or subfolders
+def file_search(file_name):
+    for root_path, _, files in os.walk("."): #os.walk is going to essentially walk me through the file structure starting at the current file directory and going down
+        if file_name in files: # Checking if the file is in the current folder and searching down to the subfolders
+            return os.path.join(root_path, file_name)  #Will combine various path components with the propert directory seperator /
+    return None # None is returned upon a file not being inside the current file structure or sub structures
+
+def main():
+    if len(sys.argv) != 3:
+        sys.exit(1)
+
+    key_filename = sys.argv[1]
+    plaintext_filename = sys.argv[2]
+
+    key_filepath = file_search(key_filename)
+    plaintext_filepath = file_search(plaintext_filename)
+
+    key_matrix = key_matrix_array(key_filepath)
+    plaintext = read_plaintext(plaintext_filepath)
+    plaintext = pad_plaintext(plaintext, len(key_matrix))
+    ciphertext = encrypt(key_matrix, plaintext)
+
+    print("Key matrix:")
+    for row in key_matrix:
+        print("   " + "   ".join(map(str, row)))
+
+    print("\nPlaintext:")
+    for i in range(0, len(plaintext), 80):
+        print(plaintext[i:i+80])
+
+    print("\nCiphertext:")
+    chunk_size = 80
+    for i in range(0, len(ciphertext), chunk_size):
+        print(ciphertext[i:i+chunk_size])
+
+
+if __name__ == "__main__":
+    main()
+
+
+'''=============================================================================
+| I Morgan Gravelat (mo870937) affirm that this program is
+| entirely my own work and that I have neither developed my code together with
+| any another person, nor copied any code from any other person, nor permitted
+| my code to be copied or otherwise used by any other person, nor have I
+| copied, modified, or otherwise used programs created by others. I acknowledge
+| that any violation of the above terms will be treated as academic dishonesty.
++============================================================================='''
--- Python/pa01.py
+++ Python/pa01.py
@@ -1,3 +1,99 @@
+'''============================================================================
+| Assignment: pa01 - Encrypting a plaintext file using the Hill cipher
+|
+| Author: Morgan Gravelat
+| Language: python
+|
+| To Execute: python3 pa01.py kX.txt pX.txt
+| where kX.txt is the keytext file
+| and pX.txt is plaintext file
+| Note:
+| All input files are simple 8 bit ASCII input
+| All execute commands above have been tested on Eustis
+|
+| Class: CIS3360 - Security in Computing - Fall 2023
+| Instructor: McAlpin
+| Due Date: October 8th, 2023
++==========================================================================='''
+
+import sys
+import os
+
+def key_matrix_array(file_name):
+    file = open(file_name, 'r')
+    n = int(file.readline())
+    key_matrix = []
+    for _ in range(n):
+        row = list(map(int, file.readline().split()))
+        key_matrix.append(row)
+    file.close()
+    return key_matrix
+
+def write_plaintext(filename):
+    with open(filename, 'r') as file:
+        plaintext = file.read()
+    cleaned_text = ''.join(char.lower() for char in plaintext if char.isalpha() and ((ord('a') <= ord(char) <= ord('z')) or (ord('A') <= ord(char) <= ord('Z'))))
+    return cleaned_text
+
+def add_padding_plaintext(plaintext, key_size):
+    padding_length = key_size - (len(plaintext) % key_size)
+    if padding_length > 0:
+        plaintext += 'x' * padding_length
+    return plaintext
+
+def hill_cipher(key_matrix, plaintext):
+    key_size = len(key_matrix)
+    ciphertext = ""
+    for i in range(0, len(plaintext), key_size):
+        block = plaintext[i:i+key_size]
+        encrypted_block = []
+        for row in key_matrix:
+            result = 0
+            for k, char in enumerate(block):
+                result += row[k] * (ord(char) - ord('a'))
+            encrypted_block.append(result % 26 + ord('a'))
+        ciphertext += ''.join([chr(char) for char in encrypted_block])
+    return ciphertext
+
+def file_search(file_name):
+    for root_path, _, files in os.walk("."):
+        if file_name in files:
+            return os.path.join(root_path, file_name)
+    return None
+
+def main():
+    if len(sys.argv) != 3:
+        sys.exit(1)
+
+    key_filename = sys.argv[1]
+    plaintext_filename = sys.argv[2]
+
+    key_filepath = file_search(key_filename)
+    plaintext_filepath = file_search(plaintext_filename)
+
+    key_matrix = key_matrix_array(key_filepath)
+    plaintext = write_plaintext(plaintext_filepath)
+    plaintext = add_padding_plaintext(plaintext, len(key_matrix))
+    ciphertext = hill_cipher(key_matrix, plaintext)
+
+    print("Key matrix:")
+    for row in key_matrix:
+        formatted_row = " "+" ".join(f"{num:3d}" for num in row)
+        print(formatted_row)
+
+    print("\nPlaintext:")
+    for i in range(0, len(plaintext), 80):
+        print(plaintext[i:i+80])
+
+    print("\nCiphertext:")
+    for i in range(0, len(ciphertext), 80):
+        print(ciphertext[i:i+chunk_size])
+
+if __name__ == "__main__":
+    main()
+
+
+
 '''=============================================================================
 | I Morgan Gravelat (mo870937) affirm that this program is
 | entirely my own work and that I have neither developed my code together with
@@ -6,91 +102,3 @@
 | copied, modified, or otherwise used programs created by others. I acknowledge
 | that any violation of the above terms will be treated as academic dishonesty.
 +============================================================================='''
-
-import sys
-import os
-
-def find_file(filename):
-    # Search for the file in the current directory and its subdirectories
-    for root, _, files in os.walk("."):
-        if filename in files:
-            return os.path.join(root, filename)
-    return None
-
-def read_key_matrix(filename):
-    with open(filename, 'r') as file:
-        n = int(file.readline())
-        key_matrix = []
-        for _ in range(n):
-            row = list(map(int, file.readline().split()))
-            key_matrix.append(row)
-    return key_matrix
-
-def read_plaintext(filename):
-    with open(filename, 'r') as file:
-        plaintext = file.read()
-    # Clean the plaintext by removing non-alphabetic characters and converting to lowercase
-    plaintext = ''.join(char.lower() for char in plaintext if char.isalpha())
-    return plaintext
-
-def pad_plaintext(plaintext, key_size):
-    padding_length = key_size - (len(plaintext) % key_size)
-    if padding_length > 0:
-        plaintext += 'x' * padding_length
-    return plaintext
-
-def encrypt(key_matrix, plaintext):
-    key_size = len(key_matrix)
-    ciphertext = ""
-    for i in range(0, len(plaintext), key_size):
-        block = plaintext[i:i+key_size]
-        encrypted_block = [0] * key_size
-        for j in range(key_size):
-            for k in range(key_size):
-                encrypted_block[j] += key_matrix[j][k] * (ord(block[k]) - ord('a'))
-            encrypted_block[j] %= 26
-            encrypted_block[j] += ord('a')
-        ciphertext += ''.join([chr(char) for char in encrypted_block])
-    return ciphertext
-
-def main():
-    if len(sys.argv) != 3: # sys.argv is the variable of the number of arguments given by the command call. Less than 3 will cause my program to exit using sys.exit with a 1 error code.
-        print("What to enter -> python3 pa01.py <key_filename> <text_filename> Make sure there are exactly 3 arguments or the program will exit") #This is asking the
-        sys.exit(1)
-
-    key_filename = sys.argv[1]
-    plaintext_filename = sys.argv[2]
-
-    # Try to find the key file and plaintext file in the current directory and subdirectories
-    key_filepath = find_file(key_filename)
-    plaintext_filepath = find_file(plaintext_filename)
-
-    if key_filepath is None:
-        print(f"Key file '{key_filename}' not found.")
-        sys.exit(1)
-
-    if plaintext_filepath is None:
-        print(f"Plaintext file '{plaintext_filename}' not found.")
-        sys.exit(1)
-
-    key_matrix = read_key_matrix(key_filepath)
-    plaintext = read_plaintext(plaintext_filepath)
-    plaintext = pad_plaintext(plaintext, len(key_matrix))
-    ciphertext = encrypt(key_matrix, plaintext)
-
-    print("Key matrix:")
-    for row in key_matrix:
-        print(" ".join(map(str, row)))
-
-    print("\nPlaintext:")
-    # Print the plaintext in lines of 80 characters or less
-    for i in range(0, len(plaintext), 80):
-        print(plaintext[i:i+80])
-
-    print("\nCiphertext:")
-    chunk_size = 80
-    for i in range(0, len(ciphertext), chunk_size):
-        print(ciphertext[i:i+chunk_size])
-
-if __name__ == "__main__":
-    main()
