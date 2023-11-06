7a1f4a38f2badd67b09c57240d20c5b96eb2804e
MorganGravelat <mgravelat@gmail.com>
2023-11-05 22:13:00 -0500

    Book reader finsihed

--- Java/BookProject/BookReader.java
+++ Java/BookProject/BookReader.java
@@ -10,3 +10,45 @@ public class BookReader {
         searchFiles(new File("."), filename, books);
         return books;
     }
+
+    private void searchFiles(File directory, String filename, List<Book> books) {
+        File[] files = directory.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    searchFiles(file, filename, books);
+                } else if (file.getName().equals(filename)) {
+                    try {
+                        String content = new String(Files.readAllBytes(file.toPath()));
+                        Book book = parseBook(content);
+                        books.add(book);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    private Book parseBook(String content) {
+        String[] lines = content.split("\n");
+        String name = "";
+        String description = "";
+        String publisher = "";
+        String text = "";
+
+        for (String line : lines) {
+            if (line.startsWith("Name:")) {
+                name = line.substring("Name:".length()).trim();
+            } else if (line.startsWith("Description:")) {
+                description = line.substring("Description:".length()).trim();
+            } else if (line.startsWith("Publisher:")) {
+                publisher = line.substring("Publisher:".length()).trim();
+            } else if (line.startsWith("Text:")) {
+                text = line.substring("Text:".length()).trim();
+            }
+        }
+
+        return new Book(name, description, publisher, text);
+    }
+}
