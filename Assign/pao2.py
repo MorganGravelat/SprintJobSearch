e1b2f90dbaabc7d960f5d1def6867f3d02eb1b81
MorganGravelat <mgravelat@gmail.com>
2023-11-09 16:13:40 -0500

    More fixing on the file system and making it work in console

--- Assign/pao2.py
+++ Assign/pao2.py
@@ -25,42 +25,43 @@ def calculate_checksum(text, checksum_size):
 
     return checksum
 
-# Function to process a file and calculate checksum
-def process_file(file_path, checksum_size):
-    with open(file_path, 'r') as file:
-        input_text = file.read()
-
-    # Echo the input text with 80 characters per row (80 characters per line)
-    for i in range(0, len(input_text), 80):
-        print(input_text[i:i + 80])
-
-    # Calculate the checksum
-    checksum = calculate_checksum(input_text, checksum_size)
-
-    # Pad the input text with 'X' characters to match the checksum size
-    while len(input_text) % (checksum_size // 4) != 0:
-        input_text += 'X'
-
-    # Output the checksum
-    print(f"{checksum_size} bit checksum is {checksum:0{checksum_size // 4}x} for all {len(input_text)} chars")
-
 # Main program
-if __name__ == "__main":
+if __name__ == "__main__":
     # Check if the correct number of command line arguments is provided
-    if len(sys.argv) != 2:
-        print("Usage: python checksum.py <checksum_size>")
+    if len(sys.argv) != 3:
+        print("Usage: python pa02.py <file_name> <checksum_size>")
         sys.exit(1)
 
-    checksum_size = int(sys.argv[1])
+    file_name = sys.argv[1]
+    checksum_size = int(sys.argv[2])
 
     # Validate the checksum size
     if checksum_size not in [8, 16, 32]:
         sys.stderr.write("Valid checksum sizes are 8, 16, or 32\n")
         sys.exit(1)
 
-    # Search for files in the current directory and process them
+    # Search for the file in the current directory and its subdirectories
     for root, _, files in os.walk('.'):
         for file in files:
-            if file.endswith(".txt"):
-                print(f"Processing file: {os.path.join(root, file)}")
-                process_file(os.path.join(root, file), checksum_size)
+            if file == file_name:
+                file_path = os.path.join(root, file)
+
+                # Open and read the input file
+                with open(file_path, 'r') as file:
+                    input_text = file.read()
+
+                # Echo the input text
+                print(input_text)
+
+                # Calculate the checksum
+                checksum = calculate_checksum(input_text, checksum_size)
+
+                # Pad the input text with 'X' characters to match the checksum size
+                while len(input_text) % (checksum_size // 4) != 0:
+                    input_text += 'X'
+
+                # Output the checksum
+                print(f"{checksum_size} bit checksum is {checksum:0{checksum_size // 4}x} for all {len(input_text)} chars")
+                break  # Found the file, no need to continue searching
+    else:
+        sys.stderr.write(f"File not found: {file_name}\n")
