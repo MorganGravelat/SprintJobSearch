9b75d8714ea5df690247eabfd9ae806b32f60ae1
MorganGravelat <mgravelat@gmail.com>
2023-11-26 23:45:59 -0500

    Wrote Staff class project final

--- Java/FinalProject/FinalProject.java
+++ Java/FinalProject/FinalProject.java
@@ -91,3 +91,50 @@ public class Faculty {
         this.rank = rank;
     }
 }
+
+public class Staff {
+    private String fullName;
+    private String id;
+    private String department; // mathematics, engineering, or english
+    private String status; // part time or full time
+
+    public Staff(String fullName, String id, String department, String status) {
+        this.fullName = fullName;
+        this.id = id;
+        this.department = department;
+        this.status = status;
+    }
+
+    // Getters and Setters
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
