6c0ba7bb21da7da976bd8bd6a35cb3abb4558cac
MorganGravelat <mgravelat@gmail.com>
2023-11-26 23:45:37 -0500

    Wrote faculty class project

--- Java/FinalProject/FinalProject.java
+++ Java/FinalProject/FinalProject.java
@@ -44,3 +44,50 @@ public class Student {
         this.numberOfCreditHours = numberOfCreditHours;
     }
 }
+
+public class Faculty {
+    private String fullName;
+    private String id;
+    private String department; // mathematics, engineering, or english
+    private String rank; // professor or adjunct
+
+    public Faculty(String fullName, String id, String department, String rank) {
+        this.fullName = fullName;
+        this.id = id;
+        this.department = department;
+        this.rank = rank;
+    }
+
+    // Getters and Setters
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    public void setRank(String rank) {
+        this.rank = rank;
+    }
+}
