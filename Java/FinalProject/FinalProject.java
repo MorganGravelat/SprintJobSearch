e15bae8be03a065359fff44ad9d479ce8b537ee9
MorganGravelat <mgravelat@gmail.com>
2023-11-27 21:36:28 -0500

    Added person and redoing the entire project

--- Java/FinalProject/FinalProject.java
+++ Java/FinalProject/FinalProject.java
@@ -1,140 +1,45 @@
-public class Student {
-    private String fullName;
-    private String id;
-    private double gpa;
-    private int numberOfCreditHours;
+import java.util.*;
 
-    public Student(String fullName, String id, double gpa, int numberOfCreditHours) {
-        this.fullName = fullName;
-        this.id = id;
-        this.gpa = gpa;
-        this.numberOfCreditHours = numberOfCreditHours;
-    }
+public class FinalProject {
 
-    // Getters and Setters
-    public String getFullName() {
-        return fullName;
-    }
+	public static void main(String[] args) {
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
 
-    public String getId() {
-        return id;
-    }
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public double getGpa() {
-        return gpa;
-    }
-
-    public void setGpa(double gpa) {
-        this.gpa = gpa;
-    }
-
-    public int getNumberOfCreditHours() {
-        return numberOfCreditHours;
-    }
-
-    public void setNumberOfCreditHours(int numberOfCreditHours) {
-        this.numberOfCreditHours = numberOfCreditHours;
-    }
 }
 
-public class Faculty {
-    private String fullName;
-    private String id;
-    private String department; // mathematics, engineering, or english
-    private String rank; // professor or adjunct
 
-    public Faculty(String fullName, String id, String department, String rank) {
-        this.fullName = fullName;
-        this.id = id;
-        this.department = department;
-        this.rank = rank;
-    }
+abstract class Person {
+	private String fullName;
+	private String id;
 
-    // Getters and Setters
-    public String getFullName() {
-        return fullName;
-    }
+	public Person(String fullName, String id) {
+		this.fullName = fullName;
+		this.id = id;
+	}
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
+	public Person(String id) {
+		fullName = "No Name";
+		this.id = id;
+	}
 
-    public String getId() {
-        return id;
-    }
+	public abstract void print();
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public String getFullName() {
+		return fullName;
+	}
 
-    public String getDepartment() {
-        return department;
-    }
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
 
-    public void setDepartment(String department) {
-        this.department = department;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public String getRank() {
-        return rank;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public void setRank(String rank) {
-        this.rank = rank;
-    }
-}
-
-public class Staff {
-    private String fullName;
-    private String id;
-    private String department; // mathematics, engineering, or english
-    private String status; // part time or full time
-
-    public Staff(String fullName, String id, String department, String status) {
-        this.fullName = fullName;
-        this.id = id;
-        this.department = department;
-        this.status = status;
-    }
-
-    // Getters and Setters
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(String department) {
-        this.department = department;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
 }
