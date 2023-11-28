3788efd7fda8fd9883dd0916d8cc5897aeeb675a
MorganGravelat <mgravelat@gmail.com>
2023-11-27 21:36:56 -0500

    Added Student extended from Person

--- Java/FinalProject/FinalProject.java
+++ Java/FinalProject/FinalProject.java
@@ -43,3 +43,43 @@ abstract class Person {
 	}
 
 }
+
+class Student extends Person {
+	private double gpa;
+	private int creditHours;
+
+	public Student(String fullName, String id, double gpa, int creditHours) {
+		super(fullName, id);
+		this.gpa = gpa;
+		this.creditHours = creditHours;
+	}
+	public Student(String id, double gpa, int creditHours) {
+		super(id);
+		this.gpa = gpa;
+		this.creditHours = creditHours;
+	}
+
+	public void print() {
+		System.out.println("Student Invoice:");
+		System.out.println("ID: " this.getId() + ", Name: " + this.getFullName() + ", GPA: " + gpa);
+		System.out.println("Tuition: " + calculateTuition());
+	}
+
+	private double calculateTuition() {
+		double baseTuition = creditHours > 0 ? 236.45 * creditHours + 52 : 0;
+		return gpa >= 3.85 ? baseTuition * 0.75 : baseTuition;
+	}
+	public double getGpa() {
+		return gpa;
+	}
+	public void setGpa(double gpa) {
+		this.gpa = gpa;
+	}
+	public int getCreditHours() {
+		return creditHours;
+	}
+	public void setCreditHours(int creditHours) {
+		this.creditHours = creditHours;
+	}
+
+}
