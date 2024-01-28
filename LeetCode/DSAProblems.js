c0b42552ab7c7d3afda1cf30b476d20185d82c0f
MorganGravelat <mgravelat@gmail.com>
2024-01-27 21:13:32 -0500

    Wednesday javascript dsa

--- LeetCode/DSAProblems.js
+++ LeetCode/DSAProblems.js
@@ -485,3 +485,33 @@ var findSubstring = function (s, words) {
     }
     return result
 };
+
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+const trap = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let maxHeightLeft = height[left];
+    let maxHeightRight = height[right];
+    let water = 0;
+    while (left < right) {
+        if (maxHeightLeft < maxHeightRight) {
+            left++;
+            if (height[left] < maxHeightLeft) {
+                water += maxHeightLeft - height[left];
+            } else {
+                maxHeightLeft = height[left];
+            }
+        } else {
+            right--;
+            if (height[right] < maxHeightRight) {
+                water += maxHeightRight - height[right];
+            } else {
+                maxHeightRight = height[right];
+            }
+        }
+    }
+    return water;
+};
