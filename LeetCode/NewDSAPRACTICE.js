50d3684a355998c01f74e6f7fd32d03de204e11f
MorganGravelat <mgravelat@gmail.com>
2024-02-01 19:47:46 -0500

    Leetcode work

--- LeetCode/NewDSAPRACTICE.js
+++ LeetCode/NewDSAPRACTICE.js
@@ -339,3 +339,63 @@ var findMedianSortedArrays = function(nums1, nums2) {
         return merged[mid];
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+    let dummyHead = new ListNode(0); // Dummy head to simplify the code
+    let current = dummyHead;
+    let carry = 0;
+
+    while (l1 || l2) {
+        let x = l1 ? l1.val : 0;
+        let y = l2 ? l2.val : 0;
+        let sum = carry + x + y;
+
+        carry = Math.floor(sum / 10);
+        current.next = new ListNode(sum % 10);
+        current = current.next;
+
+        if (l1) l1 = l1.next;
+        if (l2) l2 = l2.next;
+    }
+
+    if (carry > 0) {
+        current.next = new ListNode(carry);
+    }
+
+    return dummyHead.next;
+};
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    let charMap = {};
+    let maxLength = 0;
+    let start = 0;
+
+    for (let end = 0; end < s.length; end++) {
+        const currentChar = s[end];
+
+        if (charMap[currentChar] >= start) {
+            start = charMap[currentChar] + 1;
+        }
+        charMap[currentChar] = end;
+
+        maxLength = Math.max(maxLength, end - start + 1)
+    }
+
+    return maxLength;
+};
